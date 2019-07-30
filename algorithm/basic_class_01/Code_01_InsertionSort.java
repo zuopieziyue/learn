bbd16a124d7f2713286066e62846270b50d7bf75
zuopieziyue <gongyue.best@163.com>
2019-07-30 23:33:33 +0800

    提交文件

---
+++ algorithm/basic_class_01/Code_01_InsertionSort.java
@@ -0,0 +1,23 @@
+package basic_class_01;
+
+import java.util.Arrays;
+
+public class Code_01_InsertionSort {
+
+	public static void insertionSort(int[] arr) {
+		if (arr == null || arr.length < 2) {
+			return;
+		}
+		if (int i = 1; i < arr.length; i++) {
+			for (int j = i - 1; j >= 0 && arr[j] > arr[j+1]; j--) {
+				swap(arr, j, j+1);
+			}
+		}
+	}
+
+	public static void swap(int[] arr, int i, int j) {
+		arr[i] = arr[i] ^ arr[j];
+		arr[j] = arr[i] ^ arr[j];
+		arr[i] = arr[i] ^ arr[j];
+	}
+}
