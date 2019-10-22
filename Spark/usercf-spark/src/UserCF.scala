87e84201da65b2811623a811b606e8f28fcb1b2d
zuopieziyue <gongyue.best@163.com>
2019-10-22 18:26:44 +0800

    Signed-off-by: zuopieziyue <gongyue.best@163.com>

---
+++ Spark/usercf-spark/src/UserCF.scala
@@ -0,0 +1,2 @@
+package gongyue.cf
+
---
+++ cut/music_data/test.py
@@ -0,0 +1,32 @@
+import sys
+import jieba
+import jieba.analyse
+import pandas as pd
+
+music_meta_file = "D:\\mygithub\\data\\learn\\cut\\music_data\\music_meta"
+
+
+#读取文件
+music_meta = pd.read_csv(music_meta_file,
+						sep='\001',
+						nrows = 10,
+						names = ['item_id','item_name','item_name2','category','location','labels'])
+
+del music_meta['item_name2']
+
+#分词
+#music_meta['cut_name'] = music_meta.item_name.apply(lambda x:' '.join(jieba.cut(x, cut_all=True)))
+
+music_meta['cut_name1'] = music_meta.item_name.apply(lambda x:
+	' '.join([str(x)+'_'+str(y) for x,y in jieba.analyse.extract_tags(x, topK=6, withWeight=True)]))
+print(music_meta.head())
+
+
+
+
+
+
+
+
+
+
