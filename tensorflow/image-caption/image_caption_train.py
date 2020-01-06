4caedf0cf0006dd41ab7189b1e2597991ec88289
zuopieziyue <gongyue.best@163.com>
2020-01-06 16:30:11 +0800

    增加计算图的构建和训练流程

--- tensorflow/image-caption/image_caption_train.py
+++ tensorflow/image-caption/image_caption_train.py
@@ -248,3 +248,142 @@ pprint.pprint(batch_sentence_ids)
 pprint.pprint(batch_weights)
 pprint.pprint(batch_img_names)
 
+# 构建计算图
+def create_rnn_cell(hidden_dim, cell_type):
+	if cell_type == 'lstm':
+		return tf.contrib.rnn.BasicLSTMCell(hidden_dim, state_is_tuple=True)
+	elif cell_type == 'gru':
+		return tf.contrib.rnn.GRUCell(hidden_dim)
+	else:
+		raise Exception('%s has not been supported' % cell_type)
+
+def dropout(cell, keep_prob):
+	return tf.contrib.rnn.DropoutWrapper(cell, output_keep_prob=keep_prob)
+
+def get_train_model(hps, vocab_size, img_feature_dim):
+	num_timesteps = hps.num_timesteps
+	batch_size = hps.batch_size
+	
+	img_feature = tf.placeholder(tf.float32, (batch_size, img_feature_dim))
+	sentence = tf.placeholder(tf.int32, (batch_size, num_timesteps))
+	mask = tf.placeholder(tf.float32, (batch_size, num_timesteps))
+	keep_prob = tf.placeholder(tf.float32, name='keep_prob')
+	
+	global_step = tf.Variable(tf.zeros([], tf.int64), name='global_step', trainable=False)
+	
+	# Sets up the embedding layer.
+	embedding_initializer = tf.random_uniform_initializer(-1.0, 1.0)
+	with tf.variable_scope('embedding', initializer=embedding_initializer):
+		embeddings = tf.get_variable(
+				'embeddings',
+				[vocab_size, hps.num_embedding_nodes],
+				tf.float32)
+		embed_token_ids = tf.nn.embedding_lookup(embeddings, sentence[:, 0:num_timesteps-1])
+	
+	img_feature_embed_init = tf.uniform_unit_scaling_initializer(factor=1.0)
+	with tf.variable_scope('image_feature_embed', initializer=img_feature_embed_init):
+		embed_img = tf.layers.dense(img_feature, hps.num_embedding_nodes)
+		embed_img = tf.expand_dims(embed_img, 1)
+		embed_inputs = tf.concat([embed_img, embed_token_ids], axis=1)
+	
+	# Sets up LSTM network.
+	scale = 1.0 / math.sqrt(hps.num_embedding_nodes + hps.num_lstm_nodes[-1]) / 3.0
+	lstm_init = tf.random_uniform_initializer(-scale, scale)
+	with tf.variable_scope('lstm_nn', initializer=lstm_init):
+		cells = []
+		for i in range(hps.num_lstm_layers):
+			cell = create_rnn_cell(hps.num_lstm_nodes[i], hps.cell_type)
+			cell = dropout(cell, keep_prob)
+			cells.append(cell)
+		cell = tf.contrib.rnn.MultiRNNCell(cells)
+		
+		initial_state = cell.zero_state(hps.batch_size, tf.float32)
+		# rnn_outputs: [batch_size, num_timesteps, hps.num_lstm_node[-1]]
+		rnn_outputs, _ = tf.nn.dynamic_rnn(
+				cell,
+				embed_inputs,
+				initial_state=initial_state)
+	
+	# Sets up the fully-connected layer.
+	fc_init = tf.uniform_unit_scaling_initializer(factor=1.0)
+	with tf.variable_scope('fc', initializer=fc_init):
+		rnn_outputs_2d = tf.reshape(rnn_outputs, [-1, hps.num_lstm_nodes[-1]])
+		fc1 = tf.layers.dense(rnn_outputs_2d, hps.num_fc_nodes, name='fc1')
+		fc1_dropout = tf.contrib.layers.dropout(fc1, keep_prob)
+		fc1_dropout = tf.nn.relu(fc1_dropout)
+		
+		logits = tf.layers.dense(fc1_dropout, vocab_size, name='logits')
+	
+	# Sets up loss function.
+	with tf.variable_scope('loss'):
+		sentence_flatten = tf.reshape(sentence, [-1])
+		mask_flatten = tf.reshape(mask, [-1])
+		mask_sum = tf.reduce_sum(mask_flatten)
+		softmax_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(
+				logits = logits,
+				labels = sentence_flatten)
+		weighted_softmax_loss = tf.multiply(
+				softmax_loss,
+				tf.cast(mask_flatten, tf.float32))
+		prediction = tf.argmax(logits, 1, output_type=tf.int32)
+		correct_prediction = tf.equal(prediction, sentence_flatten)
+		correct_prediction_with_mask = tf.multiply(
+				tf.cast(correct_prediction, tf.float32),
+				mask_flatten)
+		accuracy = tf.reduce_sum(correct_prediction_with_mask) / mask_sum
+		loss = tf.reduce_sum(weighted_softmax_loss) / mask_sum
+		tf.summary.scalar('loss', loss)
+	
+	# train_op
+	with tf.variable_scope('train_op'):
+		tvars = tf.trainable_variables()
+		for var in tvars:
+			logging.info('variable name: %s' % var.name)
+		grads, _ = tf.clip_by_global_norm(
+				tf.gradients(loss, tvars),
+				hps.clip_lstm_grads)
+		for grad, var in zip(grads, tvars):
+			tf.summary.histogram('%s_grad' % var.name, grad)
+		optimizer = tf.train.AdamOptimizer(hps.learning_rate)
+		train_op = optimizer.apply_gradients(zip(grads, tvars), global_step=global_step)
+	
+	return((img_feature, sentence, mask, keep_prob),
+		(loss, accuracy, train_op),
+		global_step)
+
+placeholders, metrics, global_step = get_train_model(hps, vocab_size, img_feature_dim)
+img_feature, sentence, mask, keep_prob = placeholders
+loss, accuracy, train_op = metrics
+
+summary_op = tf.summary.merge_all()
+
+init_op = tf.global_variables_initializer()
+saver = tf.train.Saver(max_to_keep=10)
+
+# 训练流程
+training_steps = 10000
+
+with tf.Session() as sess:
+	sess.run(init_op)
+	writer = tf.summary.FileWriter(output_dir, sess.graph)
+	for i in range(training_steps):
+		batch_img_features, batch_sentence_ids, batch_weights, _ = caption_data.next_batch(hps.batch_size)
+		input_vals = (batch_img_features, batch_sentence_ids, batch_weights, hps.keep_prob)
+		
+		feed_dict = dict(zip(placeholders, input_vals))
+		fetches = [global_step, loss, accuracy, train_op]
+		
+		should_log = (i + 1) % hps.log_frequent == 0
+		should_save = (i + 1) % hps.save_frequent == 0
+		if should_log:
+			fetches += [summary_op]
+		outputs = sess.run(fetches, feed_dict)
+		global_step_val, loss_val, accuracy_val = outputs[0:3]
+		if should_log:
+			summary_str = outputs[4]
+			writer.add_summary(summary_str, global_step_val)
+			logging.info('Step: %5d, loss: %3.3f, accuracy: %3.3f' % (global_step_val, loss_val, accuracy_val))
+		if should_save:
+			logging.info("Step: %d, image caption model saved" % (global_step_val))
+			saver.save(sess, os.path.join(output_dir, 'image_caption'), global_step=global_step_val)
+
