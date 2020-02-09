4cf9db1fd3abe874acea1117d2190f16a6c3bb20
zuopieziyue <gongyue.best@163.com>
2020-02-10 00:50:33 +0800

    提交代码

--- python/movie_project/app/home/views.py
+++ python/movie_project/app/home/views.py
@@ -1,6 +1,19 @@
 # coding:utf8
 from . import home
+from flask import render_template, redirect, url_for
+
 
 @home.route("/")
 def index():
-    return "<h1 style='color:green'>this is home</h1>"
+    return render_template("home/index.html")
+
+
+@home.route("/login/")
+def login():
+    return render_template("home/login.html")
+
+
+@home.route("/logout/")
+def logout():
+    return redirect(url_for("home.login"))
+
--- python/movie_project/app/models.py
+++ python/movie_project/app/models.py
@@ -1,10 +1,10 @@
 # coding:utf8
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import datetime
+from datetime import datetime
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:root@127.0.0.1:8889/movie"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root@127.0.0.1:8889/movie"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 db = SQLAlchemy(app)
@@ -23,6 +23,8 @@ class User(db.Model):
     addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)  # 注册时间
     uuid = db.Column(db.String(255), unique=True)  # 唯一标识符
     userlogs = db.relationship("UserLog", backref='user')  # 会员日志外键关系关联
+    comments = db.relationship("Comment", backref='user')  # 评论外键关系关联
+    moviecols = db.relationship("Moviecol", backref='user')  # 电影收藏外键关系关联
 
     def __repr__(self):
         return "<User %r>" % self.name
@@ -68,11 +70,14 @@ class Movie(db.Model):
     release_time = db.Column(db.Date)  # 上映时间
     length = db.Column(db.String(100))  # 播放时间
     addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)  # 添加时间
+    comments = db.relationship("Comment", backref='movie')  # 评论外键关系关联
+    moviecols = db.relationship("Moviecol", backref='movie')  # 电影收藏外键关系关联
 
     def __repr__(self):
         return "<Movie %r>" % self.title
 
 
+# 预告
 class Preview(db.Model):
     __tablename__ = "review"
     id = db.Column(db.Integer, primary_key=True)  # 编号
@@ -82,3 +87,98 @@ class Preview(db.Model):
 
     def __repr__(self):
         return "<Preview %r>" % self.title
+
+
+# 评论
+class Comment(db.Model):
+    __tablename__ = "comment"
+    id = db.Column(db.Integer, primary_key=True)  # 编号
+    content = db.Column(db.Text)  # 内容
+    movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'))  # 所属电影
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # 所属用户
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)  # 添加时间
+
+    def __repr__(self):
+        return "<Comment %r>" % self.id
+
+
+# 电影收藏
+class Moviecol(db.Model):
+    __tablename__ = "moviecol"
+    id = db.Column(db.Integer, primary_key=True)  # 编号
+    movie_id = db.Column(db.Integer, db.ForeignKey('movie.id'))  # 所属电影
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # 所属用户
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)  # 添加时间
+
+    def __repr__(self):
+        return "<Moviecol %r>" % self.id
+
+
+# 权限
+class Auth(db.Model):
+    __tablename__ = "auth"
+    id = db.Column(db.Integer, primary_key=True)  # 编号
+    name = db.Column(db.String(100), primary_key=True)  # 名称
+    url = db.Column(db.String(255), primary_key=True)  # 地址
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)  # 添加时间
+
+    def __repr__(self):
+        return "<Auth %r>" % self.name
+
+
+# 角色
+class Role(db.Model):
+    __tablename__ = "role"
+    id = db.Column(db.Integer, primary_key=True)  # 编号
+    name = db.Column(db.String(100), primary_key=True)  # 名称
+    auths = db.Column(db.String(600))  # 地址
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)  # 添加时间
+    admins = db.relationship("Admin", backref='role')  # 管理员外键关系关联
+
+    def __repr__(self):
+        return "<Role %r>" % self.name
+
+
+# 管理员
+class Admin(db.Model):
+    __tablename__ = "admin"
+    id = db.Column(db.Integer, primary_key=True)  # 编号
+    name = db.Column(db.String(100), primary_key=True)  # 管理员账户
+    pwd = db.Column(db.String(100))  # 管理员密码
+    is_super = db.Column(db.SmallInteger)  # 是否为超级管理员， 0为超级管理员
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))  # 所属角色
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)  # 添加时间
+    adminlogs = db.relationship("Adminlog", backref="admin")  # 管理员登录日志外键关系关联
+    oplogs = db.relationship("Oplog", backref='admin')  # 管理员操作日志外键关系关联
+
+    def __repr__(self):
+        return "<Admin %r>" % self.name
+
+
+# 管理员登录日志
+class Adminlog(db.Model):
+    __tablename__ = "adminlog"
+    id = db.Column(db.Integer, primary_key=True)  # 编号
+    admin_id = db.Column(db.Integer, db.ForeignKey("admin.id"))  # 所属管理员
+    ip = db.Column(db.String(100))  # 登录IP
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)  # 登录时间
+
+    def __repr__(self):
+        return "<AdminLog %r>" % self.id
+
+
+# 操作日志
+class Oplog(db.Model):
+    __tablename__ = "oplog"
+    id = db.Column(db.Integer, primary_key=True)  # 编号
+    admin_id = db.Column(db.Integer, db.ForeignKey("admin.id"))  # 所属管理员
+    ip = db.Column(db.String(100))  # 登录IP
+    reason = db.Column(db.String(600))  # 操作原因
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)  # 登录时间
+
+    def __repr__(self):
+        return "<OpLog %r>" % self.id
+
+
+if __name__ == '__main__':
+    db.create_all()
