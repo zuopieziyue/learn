971617b3869b04835616aec751989540eb31d5fa
zuopieziyue <gongyue.best@163.com>
2020-02-09 22:49:39 +0800

    增肌spark的代码

---
+++ Spark/pyspark_learn/project/spark.py
@@ -0,0 +1,67 @@
+# encoding = 'utf-8'
+
+import sys
+
+sys.path.append("/home/hadoop/app/spark-2.3.0-bin-2.6.0-cdh5.7.0/python/")
+
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+from pyspark.sql.functions import udf
+
+if __name__ == '__main__':
+    spark = SparkSession.builder.appName("spark").getOrCreate()
+
+    data2015 = spark.read.format("csv").option("header", "true").option("inferSchema", "true") \
+        .load("file:///home/hadoop/data/Beijing_2015_HourlyPM25_created20160201.csv") \
+        .select("Year", "Month", "Day", "Hour", "Value", "QC Name")
+
+    data2016 = spark.read.format("csv").option("header", "true").option("inferSchema", "true") \
+        .load("file:///home/hadoop/data/Beijing_2016_HourlyPM25_created20170201.csv") \
+        .select("Year", "Month", "Day", "Hour", "Value", "QC Name")
+
+    data2017 = spark.read.format("csv").option("header", "true").option("inferSchema", "true") \
+        .load("file:///home/hadoop/data/Beijing_2017_HourlyPM25_created20170803.csv") \
+        .select("Year", "Month", "Day", "Hour", "Value", "QC Name")
+
+    def get_grade(value):
+        if value <= 50 and value >= 0:
+            return "健康"
+        elif value <= 100:
+            return "中等"
+        elif value <= 150:
+            return "对敏感人群不健康"
+        elif value <= 200:
+            return "不健康"
+        elif value <= 300:
+            return "非常不健康"
+        elif value <= 500:
+            return "危险"
+        elif value > 500:
+            return "爆表"
+        else:
+            return None
+
+    # 定义一个udf
+    grade_function_udf = udf(get_grade, StringType())
+
+    # 进去一个value，出来一个grade
+    group2015 = data2015.withColumn("Grade", grade_function_udf(data2015["Value"])).groupBy("Grade").count()
+    group2016 = data2016.withColumn("Grade", grade_function_udf(data2016["Value"])).groupBy("Grade").count()
+    group2017 = data2017.withColumn("Grade", grade_function_udf(data2017["Value"])).groupBy("Grade").count()
+
+    grade2015 = group2015.select("Grade", "count").withColumn("percent", group2015['count']/data2015.count()*100)
+    grade2016 = group2016.select("Grade", "count").withColumn("percent", group2016['count']/data2016.count()*100)
+    grade2017 = group2017.select("Grade", "count").withColumn("percent", group2017['count']/data2017.count()*100)
+
+    grade2015.selectExpr("Grade as grade", "count", "percent").write.format("org.elasticsearch.spark.sql")\
+        .option("es.nodes", "192.168.1.5:9200").mode("overwrite").save("weather2015/pm")
+    grade2016.selectExpr("Grade as grade", "count", "percent").write.format("org.elasticsearch.spark.sql")\
+        .option("es.nodes", "192.168.1.5:9200").mode("overwrite").save("weather2016/pm")
+    grade2017.selectExpr("Grade as grade", "count", "percent").write.format("org.elasticsearch.spark.sql")\
+        .option("es.nodes", "192.168.1.5:9200").mode("overwrite").save("weather2017/pm")
+
+    grade2015.show()
+    grade2016.show()
+    grade2017.show()
+
+    spark.stop()
