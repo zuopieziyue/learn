4b7816e3132245307cb870f739afc1eda762f7d4
zuopieziyue <gongyue.best@163.com>
2023-04-09 11:46:33 +0800

    修改bug

--- pytorch/DongShouXue/attention/MultiHeadAttention.py
+++ pytorch/DongShouXue/attention/MultiHeadAttention.py
@@ -85,7 +85,7 @@ class PositionalEncoding(nn.Module):
         self.dropout = nn.Dropout(dropout)
         # 创建一个足够长的P
         self.P = torch.zeros((1, max_len, num_hiddens))
-        X = torch.arange(max_len, dtype=torch.float32).reshape(-1, 1) / torch.pow(10000, torch.arange(0, num_hiddens, dtype=torch.float32) / num_hiddens)
+        X = torch.arange(max_len, dtype=torch.float32).reshape(-1, 1) / torch.pow(10000, torch.arange(0, num_hiddens, 2, dtype=torch.float32) / num_hiddens)
         self.P[:, :, 0::2] = torch.sin(X)
         self.P[:, :, 1::2] = torch.cos(X)
 
@@ -100,7 +100,7 @@ pos_encoding = PositionalEncoding(encoding_dim, 0)
 pos_encoding.eval()
 X = pos_encoding(torch.zeros((1, num_steps, encoding_dim)))
 P = pos_encoding.P[:, :X.shape[1], :]
-d2l.polt(torch.arange(num_steps), P[0, :, 6:10].T, xlabel='Row(Position)',
+d2l.plot(torch.arange(num_steps), P[0, :, 6:10].T, xlabel='Row(Position)',
          figsize=(6, 2.5), legend=['Col %d' % d for d in torch.arange(6, 10)])
 
 
