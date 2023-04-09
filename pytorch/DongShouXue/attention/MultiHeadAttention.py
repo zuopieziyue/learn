ca7693ee92b74debbefb7dc4fda0f866200dc442
zuopieziyue <gongyue.best@163.com>
2023-04-09 11:22:22 +0800

    新增代码

--- pytorch/DongShouXue/attention/MultiHeadAttention.py
+++ pytorch/DongShouXue/attention/MultiHeadAttention.py
@@ -78,8 +78,29 @@ print(attention(X, Y, Y, valid_lens).shape)
 print(attention(X, Y, Y, valid_lens))
 
 
+class PositionalEncoding(nn.Module):
+    """位置编码"""
+    def __init__(self, num_hiddens, dropout, max_len=1000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(dropout)
+        # 创建一个足够长的P
+        self.P = torch.zeros((1, max_len, num_hiddens))
+        X = torch.arange(max_len, dtype=torch.float32).reshape(-1, 1) / torch.pow(10000, torch.arange(0, num_hiddens, dtype=torch.float32) / num_hiddens)
+        self.P[:, :, 0::2] = torch.sin(X)
+        self.P[:, :, 1::2] = torch.cos(X)
+
+    def forward(self, X):
+        X = X + self.P[:, :X.shape[1], :].to(X.device)
+        return self.dropout(X)
 
 
-
+"""位置编码测试"""
+encoding_dim, num_steps = 32, 60
+pos_encoding = PositionalEncoding(encoding_dim, 0)
+pos_encoding.eval()
+X = pos_encoding(torch.zeros((1, num_steps, encoding_dim)))
+P = pos_encoding.P[:, :X.shape[1], :]
+d2l.polt(torch.arange(num_steps), P[0, :, 6:10].T, xlabel='Row(Position)',
+         figsize=(6, 2.5), legend=['Col %d' % d for d in torch.arange(6, 10)])
 
 
